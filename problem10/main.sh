33018230092f68ada144904a7ec59006bdf65467
Andrew Plummer <plummer574@gmail.com>
2014-06-06 09:00:18 +0100

    Correct build script for 7 and 10

--- problem10/main.sh
+++ problem10/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ problem10.cpp -o problem10.out -I../common/cpp ../common/cpp/prime.o && ./problem10.out
+g++ -c ../common/cpp/prime.cpp -o ../common/cpp/prime.o && g++ problem10.cpp -o problem10.out -I../common/cpp ../common/cpp/prime.o && ./problem10.out
--- problem7/main.sh
+++ problem7/main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-g++ problem7.cpp -o problem7.out -I../common/cpp ../common/cpp/prime.o && ./problem7.out
+
+g++ -c ../common/cpp/prime.cpp -o ../common/cpp/prime.o && g++ problem7.cpp -o problem7.out -I../common/cpp ../common/cpp/prime.o && ./problem7.out
