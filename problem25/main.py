ee912b2a136f3e6f668b864de827488231a19c61
Andrew Plummer <plummer574@gmail.com>
2014-06-07 19:12:26 +0100

    Add Problem 25

---
+++ problem25/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from functools import lru_cache
+from itertools import count
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def main():
+    strlen = 1000
+
+    for val in count(1):
+        fibval = fib(val)
+        if len(str(fibval)) >= strlen:
+            break;
+
+    print(val)
+
+
+if __name__ == '__main__':
+    main()
