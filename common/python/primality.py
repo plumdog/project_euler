02372a87ae5b2519ccbc6fea89b0bb8de5b27a23
Andrew Plummer <plummer574@gmail.com>
2014-06-10 22:24:25 +0100

    Move prime factor finder to primality lib

--- common/python/primality.py
+++ common/python/primality.py
@@ -1,5 +1,6 @@
 import math
 from functools import lru_cache
+from collections import defaultdict
 
 
 @lru_cache(maxsize=None)
@@ -9,3 +10,30 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+
+def prime_factors(num):
+    if num <= 1:
+        return defaultdict(int)
+
+    prime_divs = defaultdict(int)
+
+    trial = 2
+    num_ = num
+    top = int(math.sqrt(num_))
+
+    while True:
+        if trial > top:
+            d = defaultdict(int)
+            d[num_] = 1
+            return d
+
+        if num_ % trial == 0:
+            parent = prime_factors(num_ // trial)
+            parent[trial] += 1
+            return parent
+        else:
+            if trial == 2:
+                trial += 1
+            else:
+                trial += 2
--- problem12/main.py
+++ problem12/main.py
@@ -1,39 +1,15 @@
 #!/usr/bin/env python3
 
-import math
+import sys
+sys.path.insert(0, '../common/python/')
+
 import itertools
-from collections import defaultdict
 from operator import mul
 from functools import reduce
 
+from primality import prime_factors
+
 NUM = 500
-
-
-def prime_factors(num):
-    if num <= 1:
-        return defaultdict(int)
-
-    prime_divs = defaultdict(int)
-
-    trial = 2
-    num_ = num
-    top = int(math.sqrt(num_))
-
-    while True:
-        if trial > top:
-            d = defaultdict(int)
-            d[num_] = 1
-            return d
-
-        if num_ % trial == 0:
-            parent = prime_factors(num_ // trial)
-            parent[trial] += 1
-            return parent
-        else:
-            if trial == 2:
-                trial += 1
-            else:
-                trial += 2
 
 
 def triangle(num):
