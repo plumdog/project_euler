b111db068ab71c13257cf9a23bcdab12e8c08111
Andrew Plummer <plummer574@gmail.com>
2014-06-11 13:26:21 +0100

    Fix Problem 50

--- problem50/main.py
+++ problem50/main.py
@@ -6,30 +6,39 @@ sys.path.insert(0, '../common/python/')
 from primality import prime_sieve
 
 
-def int_permutations(num):
-    for perm in permutations(str(num)):
-        out = int(''.join(perm))
-        if out > num:
-            yield out
+def max_len_prime(max_):
+    primes = list(sorted(prime_sieve(max_)))
+
+    num_primes = len(primes)
+    max_len = 1
+    max_len_prime = None
+    for length in range(21, 10000):
+        trial = None
+        for start in range(num_primes - length):
+            # if we have an even number of terms and 2 is not
+            # included, then skip
+            if (start != 0) and (length % 2 == 0):
+                break
+
+            trial = sum(primes[start:start+length])
+
+            if trial >= max_:
+                # if the max has been reached, but we're still at the
+                # start, then all done.
+                if start == 0:
+                    return max_len_prime
+                else:
+                    break
+            elif (trial in primes) and (length > max_len):
+                max_len = length
+                max_len_prime = trial
+                break
+
 
 def main():
     max_ = 1000000
-    primes = list(sorted(prime_sieve(max_)))
-    max_len = 536
-    max_trial_len = 4000
-    max_len_prime = None
-    for length in range(max_len, 538):
-        for start in range(max_):
-            trial = sum(primes[start:start+length])
-            if trial in primes and (length > max_len):
-                max_len = length
-                max_len_prime = trial
-                print(max_len_prime)
-                #return
-            elif trial > max_:
-                continue
-    
-                
+    print(max_len_prime(max_))
+
 
 if __name__ == '__main__':
     main()
