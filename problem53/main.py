78518f2460eb8db38bf757ad9c6d3f87526e8587
Andrew Plummer <plummer574@gmail.com>
2014-07-04 22:01:59 +0100

    Add Problem 53

---
+++ problem53/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from functools import lru_cache
+import math
+
+@lru_cache(maxsize=None)
+def factorial(n):
+    return math.factorial(n)
+
+def choose(n, r):
+    return factorial(n) / (factorial(r) * factorial(n-r))
+
+def main():
+    max_ = 1000000
+    upto = 100
+    count = 0
+    for n in range(1, upto+1):
+        for r in range(n):
+            if choose(n, r) > max_:
+                count += 1
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
