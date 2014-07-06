518c49ebf0a4430ff3301d42fb9e47a9a28d68ac
Andrew Plummer <plummer574@gmail.com>
2014-07-06 14:17:26 +0100

    Fix java Fraction class and Problem 33

--- common/java/fraction/Fraction.java
+++ common/java/fraction/Fraction.java
@@ -1,10 +1,18 @@
 package fraction;
 
-class Fraction {
+public class Fraction {
 	public final int num, den;
 	public Fraction(int num, int den) {
 		this.num = num;
 		this.den = den;
+	}
+
+	public int num() {
+		return num;
+	}
+
+	public int den() {
+		return den;
 	}
 
 	public boolean isEqual(Fraction other) {
@@ -15,10 +23,22 @@ class Fraction {
 		int newNum = this.num;
 		int newDen = this.den;
 
-		for(int i = this.den - 1; i >= 2; --i) {
-			if((newNum % i == 0) && (newDen % i == 0)) {
-				newNum /= i;
-				newDen /= i;
+		int trial = 2;
+		while(true) {
+			if((newNum % trial == 0) && (newDen % trial == 0)) {
+				newNum /= trial;
+				newDen /= trial;
+				trial = 2;
+				continue;
+			}
+
+			if (trial == 2) {
+				trial = 3;
+			} else {
+				trial += 2;
+			}
+
+			if ((trial > newNum) || (trial > newDen)) {
 				break;
 			}
 		}
@@ -26,7 +46,33 @@ class Fraction {
 		return new Fraction(newNum, newDen);
 	}
 
+	/*
+	 * a/b * c/d = ac/bd
+	 */
 	public Fraction times(Fraction other) {
 		return new Fraction(this.num * other.num, this.den * other.den);
+	}
+
+	/*
+	 * a/b + c/d = (ad + cb) / bd
+	 */
+	public Fraction add(Fraction other) {
+		return new Fraction(this.num * other.den + other.num * this.den, this.den * other.den);
+	}
+
+	public Fraction subtract(Fraction other) {
+		return add(other.negative());
+	}
+
+	public Fraction negative() {
+		return new Fraction(-num, den);
+	}
+
+	public Fraction inverse() {
+		return new Fraction(den, num);
+	}
+
+	public String toString() {
+		return num + "/" + den;
 	}
 }
--- problem33/Problem33.java
+++ problem33/Problem33.java
@@ -1,15 +1,24 @@
 import fraction.Fraction;
 
 class FakeCancelFraction extends Fraction{
-	public Fraction fakeCancel() {
+	public FakeCancelFraction(int num, int den) {
+		super(num, den);
+	}
+
+	public FakeCancelFraction(Fraction fr) {
+		super(fr.num(), fr.den());
+	}
+
+	public FakeCancelFraction fakeCancel() {
 		char[] numChars = Integer.toString(num).toCharArray();
 		char[] denChars = Integer.toString(den).toCharArray();
 
 		for(int numi = 0; numi <= 1; ++numi) {
 			for(int deni = 0; deni <= 1; ++deni) {
 				if((numChars[numi] == denChars[deni]) && numChars[numi] != '0') {
-					return new Fraction(Character.getNumericValue(numChars[1-numi]),
-										Character.getNumericValue(denChars[1-deni]));
+					return new FakeCancelFraction(
+						new Fraction(Character.getNumericValue(numChars[1-numi]),
+									 Character.getNumericValue(denChars[1-deni])));
 				}
 			}
 		}
@@ -21,21 +30,21 @@ class FakeCancelFraction extends Fraction{
 
 public class Problem33 {
 	public static void main(String[] args) {
-		Fraction fr = null;
-		Fraction canFr = null;
-		Fraction prodFr = new FakeCancelFraction(1, 1);
+		FakeCancelFraction fr = null;
+		FakeCancelFraction canFr = null;
+		FakeCancelFraction prodFr = new FakeCancelFraction(1, 1);
 
 		for(int num = 11; num <= 99; ++num) {
 			for(int den = num + 1; den <= 98; ++den) {
 				fr = new FakeCancelFraction(num, den);
 				canFr = fr.fakeCancel();
 				if((canFr != null) && fr.isEqual(canFr)) {
-					prodFr = prodFr.times(fr);
+					prodFr = new FakeCancelFraction(prodFr.times(fr));
 				}
 			}
 		}
 
-		prodFr = prodFr.lowestTerms();
-		System.out.println(prodFr.den);
+		prodFr = new FakeCancelFraction(prodFr.lowestTerms());
+		System.out.println(prodFr.den());
 	}
 }
