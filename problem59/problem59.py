6941a64b5ee01ca6c3bb36e785fa05c9612ef5aa
Andrew Plummer <plummer574@gmail.com>
2014-07-10 22:06:08 +0100

    Simplify Problem59 to remove dependency on dictionary library

--- problem59/problem59.py
+++ problem59/problem59.py
@@ -1,54 +1,30 @@
 #!/usr/bin/env python3
 
-import enchant
 import sys
 import string
 import itertools
 
 def main():
-    d = enchant.Dict("en_GB")
-
     with open(sys.argv[1]) as f:
-        contents = f.readlines()[0]
-    nums = [int(c) for c in contents.split(',')]
+        nums = [int(c) for c in f.readline().split(',')]
 
     for pw in itertools.product(string.ascii_lowercase, repeat=3):
-        decrypt_nums = decrypt(nums, pw)
-        text = nums_to_text(decrypt_nums)
-        if(is_english(d, text)):
-            print(sum(decrypt(nums, pw)))
+        decrypted_nums = decrypt(nums, pw)
+
+        """If a string contains the word 'the', then its English. We could use
+        a proper dictionary or some knowledge of ngrams, but this
+        works.
+        """
+
+        if 'the' in ''.join(chr(n) for n in decrypted_nums).split():
+            print(sum(decrypted_nums))
             return
 
-    
+
 def decrypt(nums, password):
     pwlen = len(password)
-    return (n ^ ord(password[i % pwlen]) for i, n in enumerate(nums))
-
-
-def nums_to_text(nums):
-    return ''.join(chr(n) for n in nums)
-    
-
-def is_english(d, text):
-    """If more than 75% of the text is recognised as English, then that's
-    good enough.
-    """
-    yes = 0
-    no = 0
-    for w in text.split():
-        if d.check(w):
-            yes += 1
-        else:
-            no += 1
-
-        if yes + no > 10:
-            if yes < no:
-                return False
-
-    return yes > (3 * no)
+    return [n ^ ord(password[i % pwlen]) for i, n in enumerate(nums)]
 
 
 if __name__ == '__main__':
-    #import cProfile
-    #cProfile.run('main()')
     main()
