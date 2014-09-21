b3bb9d3ba4d0b1cd1e4c9633ae2b038ec55e81b1
Andrew Plummer <plummer574@gmail.com>
2014-09-21 16:25:29 +0100

    Add rudimentary lookup script

---
+++ tools/lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://projecteuler.net'
+
+def lookup(problem_number):
+    resp = requests.get(URL + '/problem=' + str(problem_number))
+    html = resp.content.decode('utf-8')
+
+    bs = BeautifulSoup(html)
+    content = bs.find('div', 'problem_content')
+    return content.get_text().strip()
+
+
+def main():
+    import sys
+    try:
+        num = sys.argv[1]
+    except IndexError:
+        num = 1
+
+    print(lookup(num))
+
+
+if __name__ == '__main__':
+    main()
