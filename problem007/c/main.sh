c4f760f0736eae328f2a668b07d4124ddee264da
Andrew Plummer <plummer574@gmail.com>
2015-11-04 20:25:04 +0000

    Tidy long gcc commands in bash

--- problem007/c/main.sh
+++ problem007/c/main.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-gcc -c ../../common/c/prime.c -o ../../common/c/prime.o -std=c99 && gcc problem7.c -o problem7.out -I../../common/c ../../common/c/prime.o -lm -std=c99 && ./problem7.out
+prime_options=(
+    -c ../../common/c/prime.c
+    -o ../../common/c/prime.o
+    -std=c99
+)
+
+main_options=(
+    -o problem7.out
+    -I../../common/c ../../common/c/prime.o
+    -lm -std=c99
+)
+
+gcc "${prime_options[@]}" && gcc problem7.c "${main_options[@]}" && ./problem7.out
