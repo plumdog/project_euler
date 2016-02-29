514c747a6bf1892140eaa6ff8287ca73c5629e85
Andrew Plummer <plummer574@gmail.com>
2016-02-29 23:23:06 +0000

    Rework Problem 23 in Java

--- problem023/java/Problem23.java
+++ problem023/java/Problem23.java
@@ -1,23 +1,23 @@
 import java.util.HashSet;
 
 public class Problem23 {
-	public static HashSet<Integer> abundants = new HashSet<Integer>();
-
-	public static void buildAbundants(int max) {
+	public static HashSet<Integer> getAbundants(int max) {
+		HashSet<Integer> abundants = new HashSet<Integer>();
 		for(int i = 1; i <= max; ++i) {
 			if(isAbundant(i)) {
 				abundants.add(i);
 			}
 		}
+		return abundants;
 	}
 	
 	public static void main(String[] args) {
 		int max = 28123;
-		buildAbundants(max);
+		HashSet<Integer> abundants = getAbundants(max);
 
 		int total = 0;
 		for(int j = 1; j <= max; ++j) {
-			if(!sumOfTwoAbundants(j)) {
+			if(!sumOfTwoAbundants(j, abundants)) {
 				total += j;
 			}
 		}
@@ -25,7 +25,7 @@ public class Problem23 {
 		System.out.println(total);
 	}
 
-	public static boolean sumOfTwoAbundants(int num) {
+	public static boolean sumOfTwoAbundants(int num, HashSet<Integer> abundants) {
 		for(int ab : abundants) {
 			if(abundants.contains(num - ab)) {
 				return true;
