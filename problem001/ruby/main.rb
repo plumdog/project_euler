5c2656d302ecbfdd46628cc2425660318a1311cf
Andrew Plummer <plummer574@gmail.com>
2016-07-12 20:17:14 +0100

    Tidy Problem 1 in Ruby

--- problem001/ruby/main.rb
+++ problem001/ruby/main.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/ruby
-
-result = (1...1000).select do |i|
-    (i % 3 == 0) or (i % 5 == 0)
-end
-puts result.reduce(0, :+)
+puts (1...1000).select { |i| (i % 3).zero? or (i % 5).zero? }.reduce(0, :+)
