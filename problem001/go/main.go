5e8c153b53e23f63923e6757422caf2072561412
Andrew Plummer <plummer574@gmail.com>
2016-07-12 20:41:14 +0100

    Problem 1 in Go

---
+++ problem001/go/main.go
@@ -0,0 +1,14 @@
+//usr/bin/go run $0 $@; exit
+
+package main
+import "fmt"
+
+func main() {
+    total := 0;
+    for i := 0; i < 1000; i++ {
+		if (i % 3 == 0) || (i % 5 == 0) {
+			total += i;
+		}
+	}
+	fmt.Println(total);
+}
