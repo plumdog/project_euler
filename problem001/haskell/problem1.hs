848099554194a416c889c9a3abba70adefd6f9b7
Andrew Plummer <plummer574@gmail.com>
2016-07-12 15:32:35 +0100

    Tidy Problem 1 in Haskell

--- problem001/haskell/problem1.hs
+++ problem001/haskell/problem1.hs
@@ -1,6 +1,2 @@
 mult3or5 num = (mod num 3 == 0) || (mod num 5 == 0)
-lessthan a b = a > b
-
-problem1 upto = sum $ takeWhile (lessthan upto) (filter mult3or5 [0..])
-
-main = print $ problem1 1000
+main = print $ sum $ takeWhile (< 1000) (filter mult3or5 [0..])
